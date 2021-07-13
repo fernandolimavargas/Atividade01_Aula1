852bb8e60c0e63dbd0e84b8dcbed41863bf3775b
Fernando Vargas <fervargas2210@gmail.com>
2021-07-13 13:19:08 -0300

    Create main.py
    Trabalho pronto.

---
+++ main.py
@@ -0,0 +1,37 @@
+'''
+Construir um algoritmo que contenha 3 listas:
+•  Nomes de produtos
+•  Preços de cada produto
+•  Quantidades de cada produto
+Construir uma função para imprimir um dos produtos da lista e uma função para retirar um dos produtos das listas
+
+Forma de entrega desta atividade: Crie um repositório remoto Git no GitHub, GitLab ou Bitbucket e poste aqui apenas o link do repositório que contém o código que você desenvolveu.
+'''
+
+lst_produto = []
+lst_preco  = []
+lst_quantidade = []
+
+
+class Produto: 
+    def __init__(self, produto, preco, quantidade): 
+        self.produto = produto 
+        self.preco = preco 
+        self.quantidade = quantidade
+    
+    def imprimir(self): 
+        print(f'Produto: {self.produto}\nPreço: {self.preco}\nQuantidade: {self.quantidade}')
+    
+
+a1 = Produto('Coca-Cola', 10, 15)
+a1.imprimir()
+'''print(f'Produto: {a1.produto}\nPreço: {a1.preco}\nQuantidade: {a1.quantidade}')'''
+
+
+lst_produto.append(a1.produto) 
+lst_preco.append(a1.preco)
+lst_quantidade.append(a1.quantidade)
+
+print(lst_produto)
+print(lst_preco)
+print(lst_quantidade)
